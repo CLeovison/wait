f71f4af4fd5c6f638572a4d7766693258aba9eed
Leovison <cleovison@gmail.com>
2024-02-08 12:38:38 +0800

    Added A Modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/UI/Navbar";
-
+import { Helmet } from "react-helmet";
 //End of React Modules
-
 
 //Start of Pages
 import Home from "./pages/Home";
@@ -13,16 +12,17 @@ import Shop from "./pages/Categories";
 import Contact from "./pages/Contacts";
 //End of Pages
 
-
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-     
+        <Helmet>
+          <title>We're All In Time - WAIT</title>
+        </Helmet>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About/>}></Route>
+          <Route path="/about" element={<About />}></Route>
           <Route path="/categories" element={<Shop />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
         </Routes>
